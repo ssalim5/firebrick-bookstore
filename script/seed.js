af87389be7bfd80e787020c77638f6e3c8993172
Don Romaniello <don.romaniello@gmail.com>
2022-04-19 12:53:10 -0400

    Book model added, basic use of book-api module.

--- script/seed.js
+++ script/seed.js
@@ -12,8 +12,8 @@ async function seed() {
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
+    User.create({ username: 'cody', password: '123', email: 'cody@pug.com' }),
+    User.create({ username: 'murphy', password: '123', email: 'murphy@beds.com' }),
   ])
 
   console.log(`seeded ${users.length} users`)
--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,19 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Book = require('./models/Book')
 
 //associations could go here!
+
+User.hasMany(Book);
+Book.hasMany(User)
+
+
 
 module.exports = {
   db,
   models: {
     User,
+    Book
   },
 }
