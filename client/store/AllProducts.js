bb1dfb51a0f345d7224f0d13da1b490566a02ba3
Justin Chiu <justinchiu35@gmail.com>
2022-04-20 10:57:00 -0400

    Add/Delete books

--- client/store/AllProducts.js
+++ client/store/AllProducts.js
@@ -3,11 +3,34 @@ import axios from "axios";
 const initialState = [];
 
 const SET_BOOKS = "SET_BOOKS";
+const ADD_BOOK = "ADD_BOOK";
 
 const _setBooks = (books) => {
   return {
     type: SET_BOOKS,
     books
+  };
+};
+
+const _addBook = (books) => {
+  return {
+    type: ADD_BOOK,
+    books
+  };
+};
+
+export const addBook = (book, user) => {
+  return async (dispatch) => {
+    if (user.admin) {
+      try {
+        const {data} =  await axios.post("/api/books",book);
+        dispatch(_addBook(data));
+      } catch (err){
+        console.log(err);
+      }
+    } else {
+      //Nothing --> Page that denies
+    }
   };
 };
 
@@ -27,6 +50,12 @@ export default (state = initialState, action) => {
   switch(action.type) {
     case SET_BOOKS:
       return action.books;
+    case ADD_BOOK: {
+      const books = state.map( element => {
+        return element;
+      });
+      return [...books];
+    }
     default:
       return state;
   }
